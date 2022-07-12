92e870350ce162cf506dc30d9ef7701e5f43913d
Luca Gaddini <90861515+lucagad@users.noreply.github.com>
2022-07-12 17:31:52 +0200

    Aggiunta lista pizze nella vista della Show degli ingredienti

--- resources/views/admin/ingredients/show.blade.php
+++ resources/views/admin/ingredients/show.blade.php
@@ -5,6 +5,16 @@
         <div class="card" style="width: 18rem;">
             <div class="card-body">
               <h5 class="card-title">{{$ingredient->name}}</h5>
+
+              @if(count($ingredient->pizzas) != 0)
+                <p class="card-text"> Lista Pizze: </p>
+                <ul>
+                  @foreach ( $ingredient->pizzas as $pizza )
+                    <li class="card-text">{{$pizza->name}}</li>
+                  @endforeach
+                </ul>
+              @endif
+
             
               <a href="{{route('admin.ingredients.edit', $ingredient)}}" class="btn btn-primary">Edit</a>
               <a href="{{route('admin.ingredients.index')}}" class="btn btn-primary">Lista</a>
